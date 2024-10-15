e948b71d9ae4f6f07d865c02635344fb745b20d7
luuu026 <139782243+luuu026@users.noreply.github.com>
2024-10-15 15:49:45 +0100

    data persistence

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GameManager : MonoBehaviour
+{
+    //This is the handler of the main menu scene
+
+    [SerializeField] Text PlayerNameInput;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SetPlayerName()
+    {
+        PlayerDataHandle.Instance.PlayerName = PlayerNameInput.text;
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
